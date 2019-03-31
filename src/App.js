027ba7328db22bc6c8801b04f63e631ecc147a80
yeonghun cha <chyh3272@gmail.com>
2019-04-01 00:23:07 +0900

    feat: Add router (#5)
    add router
    
    issue: #5

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 import './App.scss';
 import TestContainer from './containers/TestContainer/TestContainer';
-
+import {Route, BrowserRouter as Router, Link} from 'react-router-dom';
+const Other = () => {
+  return (
+    <div>hello other</div>
+  )
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
-         
-        <TestContainer />
+         <Router>
+            <Link to="/">go to home</Link>
+            <Link to="/other">go to other</Link>
+            <Route exact path="/" component = {TestContainer} />
+            <Route path="/other" component = {Other} />
+          
+        </Router>
       </div>
     );
   }
