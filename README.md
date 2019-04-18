bdd4c00be9a15acf4ebf5a35fe3de40d932caec3
yeonghun cha <chyh3272@gmail.com>
2019-04-18 19:12:00 +0900

    feat: Set boilerplate
     - change project name
     - add scss styles
     - add locale for react-intl
     - set box-sizing(border-box)

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# favorite-photo
+# cha-boilerplate
 It's for person who want to get private favorite photo gallery
--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import {Route, BrowserRouter as Router, Link} from 'react-router-dom';
-import './App.css';
-// import './App.scss';
-
 
 // language / hoc
-import { compose } from 'recompose';
 import { IntlProvider, addLocaleData } from 'react-intl'
 import en from 'react-intl/locale-data/en'
 import es from 'react-intl/locale-data/es'
@@ -14,37 +10,20 @@ import * as locale from './locale';
 
 // containers
 import TestContainer from './containers/TestContainer/TestContainer';
-import HomeContainer from './containers/HomeContainer/HomeContainer';
-
-const Other = () => {
-  return (
-    <div>hello other</div>
-  )
-}
 
 addLocaleData([...en, ...es, ...pt])
 const defaultLang = navigator.language.slice(0,2) || 'en'
 
-class App extends Component {
-  componentDidMount() {
-    console.log('process.env :', process.env);
-  }
-  
+class App extends Component {  
   render() {
     return (
       <IntlProvider
-        // locale={lang}
         locale={'en'}
-        // messages={locale[defaultLang==='es'||defaultLang==='en' ? defaultLang:lang]}
         messages={locale[defaultLang==='es'||defaultLang==='en' ? defaultLang:'en']}
       >
         <div className="App">
           <Router>
-              <Link to="/">go to home</Link>
-              <Link to="/other">go to other</Link>
-              <Route path="/other" component = {Other} />
-              <Route exact path="/" component = {HomeContainer} />
-            
+              <Route exact path="/" component = {TestContainer} />
           </Router>
         </div>
       </IntlProvider>
